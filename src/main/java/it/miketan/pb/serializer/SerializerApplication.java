534e0e1eac79b447ec520ea751fe5f6b3acf71a2
miketanJp <eien.tabi@outlook.it>
2024-06-12 02:37:38 +0200

    second commit

--- src/main/java/it/miketan/pb/serializer/SerializerApplication.java
+++ src/main/java/it/miketan/pb/serializer/SerializerApplication.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 public class SerializerApplication extends Application {
    @Override
    public void start(Stage stage) throws IOException {
-
       FXMLLoader fxmlLoader = new FXMLLoader(SerializerApplication.class.getResource("main-view.fxml"));
       Scene scene = new Scene(fxmlLoader.load(), 1024, 850);
       stage.setTitle("PB Subsystem Customizer - ©2024 Miketan");
